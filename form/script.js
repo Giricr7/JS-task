8244dfdcd31a8da1eeac4560ab1d939fd95edf54
Giricr7 <90244540+Giricr7@users.noreply.github.com>
2021-09-20 22:00:54 -0600

    Update script.js

--- form/script.js
+++ form/script.js
@@ -1,1 +1,42 @@
 
+function collect_details(){
+
+    var firstname = document.getElementById('fname').value;
+    var lastname = document.getElementById('lname').value;
+    var address = document.getElementById('address').value;
+    var pin = document.getElementById('pincode').value;
+    var state = document.getElementById('state').value;
+    var country = document.getElementById('country').value;
+    var gender;
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+
+
+    if(document.getElementById('male').checked == true)
+    {
+        gender='Male'
+    }
+    else
+    {
+        gender='Female'
+    }
+
+    if(firstname=='' || lastname=='' || address=='' || pin=='' || state==''|| country=='')
+    {
+        Swal.fire({
+            icon: 'error',
+            title: 'please fill the required fields'
+          })
+    }
+    else if(checkboxes.length <2)
+        {
+            Swal.fire({
+                icon: 'error',
+                title: 'select atleast two foods'
+           
+              })
+        }
+
+
+
+
+}
