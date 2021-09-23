0c1d5d7aee8d94168f7f8412c3d51b67f4d74523
Giriprasath <giriarjun1995@gmail.com>
2021-09-22 19:31:18 -0600

    updated form

--- form/script.js
+++ form/script.js
@@ -1,16 +1,16 @@
 
 function collect_details(){
 
+// fetching values
     var firstname = document.getElementById('fname').value;
     var lastname = document.getElementById('lname').value;
+    var name=`${firstname} ${lastname}`
+    
     var address = document.getElementById('address').value;
     var pin = document.getElementById('pincode').value;
-    var state = document.getElementById('state').value;
-    var country = document.getElementById('country').value;
+    address = `${address},${pin}`
+    
     var gender;
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-
-
     if(document.getElementById('male').checked == true)
     {
         gender='Male'
@@ -20,6 +20,22 @@ function collect_details(){
         gender='Female'
     }
 
+
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    var food=[]
+    for (let i = 0; i < checkboxes.length;i++){
+        food.push(checkboxes[i].value)
+    }
+    food=food.join(',')
+    
+    
+    var state = document.getElementById('state').value;
+    var country = document.getElementById('country').value;
+    
+// collecting all values inside an array
+    var details = [name, address, gender, food, state, country]
+    
+//validation
     if(firstname=='' || lastname=='' || address=='' || pin=='' || state==''|| country=='')
     {
         Swal.fire({
@@ -34,9 +50,26 @@ function collect_details(){
                 title: 'select atleast two foods'
            
               })
+    } else {
+        
+        let tabl = document.getElementById('data_table')
+
+        let total_cells = details.length;
+        let newrow = tabl.insertRow();
+
+        let cells = details.map((element) => {
+            let new_cell = newrow.insertCell()
+            new_cell.textContent = element;
+        })
+
+
+        tabl.appendChild(newrow)
+
+        document.forms['form_details'].reset();
         }
 
 
 
-
 }
+
+
