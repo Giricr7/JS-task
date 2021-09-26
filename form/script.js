7fe3dfa32150c2435d5d1bc15c2c88c02dbb5f98
Giriprasath <giriarjun1995@gmail.com>
2021-09-26 08:02:51 -0600

    validation completed

--- form/script.js
+++ form/script.js
@@ -1,90 +1,150 @@
+if (localStorage.length === 0) {
+    localStorage.setItem('rows', '')
+    
+}
 
-function collect_details(){
 
+
+
+function collect_details() {
+    
 // fetching values
-    var firstname = document.getElementById('fname').value;
-    var lastname = document.getElementById('lname').value;
-    var name=`${firstname} ${lastname}`
-    
-    var address = document.getElementById('address').value;
-    var pin = document.getElementById('pincode').value;
-    address = `${address},${pin}`
-    
-    var gender;
-    if(document.getElementById('male').checked == true)
-    {
-        gender='Male'
-    }
-    else
-    {
-        gender='Female'
-    }
+var firstname = document.getElementById('fname').value;
+var lastname = document.getElementById('lname').value;
+var name=`${firstname} ${lastname}`
+
+var address = document.getElementById('address').value;
+var pin = document.getElementById('pincode').value;
+address = `${address},${pin}`
+
+var gender;
+if(document.getElementById('male').checked == true)
+{
+    gender='Male'
+}
+else
+{
+    gender='Female'
+}
 
 
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-    var food=[]
-    for (let i = 0; i < checkboxes.length;i++){
-        food.push(checkboxes[i].value)
-    }
-    food=food.join(',')
-    
-    
-    var state = document.getElementById('state').value;
-    var country = document.getElementById('country').value;
-    
+var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+var food=[]
+for (let i = 0; i < checkboxes.length;i++){
+    food.push(checkboxes[i].value)
+}
+food=food.join(',')
+
+
+var state = document.getElementById('state').value;
+var country = document.getElementById('country').value;
+
 // collecting all values inside an array
-    var details = [name, address, gender, food, state, country]
-    
+var details = [name, address, gender, food, state, country]
+
 //validation
-    if(firstname=='' || lastname=='' || address=='' || pin=='' || state==''|| country=='')
+if(firstname=='' || lastname=='' || address=='' || pin=='' || state==''|| country=='')
+{
+    Swal.fire({
+        icon: 'error',
+        title: 'please fill the required fields'
+      })
+}
+else if(checkboxes.length <2)
     {
         Swal.fire({
             icon: 'error',
-            title: 'please fill the required fields'
+            title: 'select atleast two foods'
+       
           })
-    }
-    else if(checkboxes.length <2)
-        {
-            Swal.fire({
-                icon: 'error',
-                title: 'select atleast two foods'
-           
-              })
+} else {
+    
+  
+
+    var tabl = document.getElementById('data_table');
+    var newrow = tabl.insertRow();
+    
+//inserting all elements to the new row
+        details.map((element) => {
+        let new_cell = newrow.insertCell()
+        new_cell.textContent = element;
+    })
+
+//appending new row
+
+    tabl.appendChild(newrow)
+//storing the data in local storage
+    if (localStorage.getItem('rows') == '') {
+        var arr = []
     } else {
-        
-        var tabl = document.getElementById('data_table')
-
-        let total_cells = details.length;
-        let newrow = tabl.insertRow();
-
-        let cells = details.map((element) => {
-            let new_cell = newrow.insertCell()
-            new_cell.textContent = element;
-        })
-
-
-
-        tabl.appendChild(newrow)
-        localStorage.setItem('table', document.getElementById('data_table').innerHTML);
-        document.forms['form_details'].reset();
-
-        
-
-        Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: 'Data added to table',
-            showConfirmButton: false,
-            timer: 2500
-          })
-        }
-
-        
-        
+        arr = JSON.parse(localStorage.getItem('rows'));
+    }
+    
+    arr.push(details);
+    localStorage['rows'] = JSON.stringify(arr);
    
 
+//clearing the input fields
+    document.forms['form_details'].reset();
+
+    
+//pop-up for successfull data insertion to table
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Data added to table',
+        showConfirmButton: false,
+        timer: 2500
+      })
+    }
+
+
+
+   
+   
 }
 
-if (location.reload) {
-    document.getElementById('data_table').innerHTML=localStorage.getItem('table')
+
+if (location.reload && localStorage.getItem('rows') !== '') {
+    
+
+//re-inserting all elements to the new row
+    let stored_values = JSON.parse(localStorage.getItem('rows'));
+
+    let table = document.getElementById('data_table');
+   
+    
+//inserting all elements to the table
+    for (let i = 0; i < stored_values.length; i++){
+        let newrow = table.insertRow();
+        stored_values[i].map((element) => {
+            let new_cell = newrow.insertCell()
+            new_cell.textContent = element;
+           
+        })
+        table.appendChild(newrow); table.appendChild(newrow)
+    }
+        
+
+//appending new row
+
+    
+    
+       
+
+//appending new row
+
+    
+}
+
+
+
+//clears the locally stored values
+function delete_data(){
+  
+    localStorage.clear();
+    location.reload()
+   
+   
+
 }
